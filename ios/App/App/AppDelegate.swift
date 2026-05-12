759bf3b7f12e1e29bfb6fe97820349da1d7ff3a2
matthiasinsidiom-create <matthias.insidiom@gmail.com>
2026-05-12 18:06:31 +0200

    Update AppDelegate.swift

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -48,4 +48,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             object: error
         )
     }
+        func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(
+            name: .capacitorDidRegisterForRemoteNotifications,
+            object: deviceToken
+        )
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(
+            name: .capacitorDidFailToRegisterForRemoteNotifications,
+            object: error
+        )
+    }
 }
